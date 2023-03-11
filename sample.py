a5e31c570c47a3abe294b14a83d28c90aae6abc8
kwankiu <ngkwankiuu@gmail.com>
2023-03-11 03:36:58 -0800

    update sample.py

--- sample.py
+++ sample.py
@@ -1,26 +1,16 @@
 from MicroTuya import OutletDevice
 
-# wi-fi settings
-ssid_ = ""
-wp2_pass = ""
-
 # smart power outlet settings
 DEVICE_ID_HERE = ""
 IP_ADDRESS = ""
 LOCAL_KEY = ""
 
-# connecting to wi-fi
-def do_connect():
-    import network
-    sta_if = network.WLAN(network.STA_IF)
-    if not sta_if.isconnected():
-        print('connecting to network...')
-        sta_if.active(True)
-        sta_if.connect(ssid_, wp2_pass)
-        while not sta_if.isconnected():
-            pass
-    print('network config:', sta_if.ifconfig())
-do_connect()
+print("Welcome to MicroTuya!")
 
 # connecting to the power outlet
 outlet = OutletDevice(DEVICE_ID_HERE, IP_ADDRESS, LOCAL_KEY)
+outlet.setVersion(3.3) # set tuya protocol version to 3.3
+
+outlet.turn_on() # turn on the power outlet
+print(outlet.status()) # status of the power outlet
+outlet.turn_off() # turn off the power outlet
